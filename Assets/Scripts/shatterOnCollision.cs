bbcb2a89bf91d9eac5eb3dc7eaf858a5315d3c29
Blackmagelvl100 <nf.mendoza912@gmail.com>
2017-07-20 10:16:59 -0500

    Arte: Anfora
    Se introduce el prefab anfora. Tiene un script para cmabiar de asset en colision y destruir el asset estatico. El estatico funcioona con un box collider el que tiene fisica tiene que tener mesh colliders en sus piezas.

---
+++ Assets/Scripts/shatterOnCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shatterOnCollision : MonoBehaviour {
+    public GameObject replacement;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject.Instantiate(replacement, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+    }
+
+}
