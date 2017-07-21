6eaceb5e8947bb022bc3c5c83c17855dc2e3ea93
JSpalas desktop <elgranpaicron@gmail.com>
2017-07-21 18:09:16 -0500

    documentacion del metodo del npc y correcion de errores del mergue anterior

--- Assets/Scripts/NpcControler.cs
+++ Assets/Scripts/NpcControler.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class NpcControler : MonoBehaviour {
 
 
-    [SerializeField] private bool lookat;
-    private Transform trans;
+    [SerializeField] private bool lookat;  // variale que define si el jugador esta en vista del npc
+    private Transform trans;  // atributo que representa la posicion del objeto
+    private GameObject player;  // el jugador
 
-    private GameObject player;
+    public float speed; // velocidad con la que el npc se aleja del pj
 	// Use this for initialization
-	void Start () {
+	void Start () {           // inicializacion de las variables
         lookat = false;
         trans = GetComponent<Transform>();
         player = GameObject.Find("Player");
@@ -19,24 +20,24 @@ public class NpcControler : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-        if(lookat)
+        if(lookat)   
         {
-            Vector3 look = new Vector3(player.transform.position.x, trans.position.y, player.transform.position.z);
-            trans.LookAt(look);
-            trans.position -= trans.forward*Time.deltaTime*2f;
+            Vector3 look = new Vector3(player.transform.position.x, trans.position.y, player.transform.position.z); //metodo que hace que el npc mire ahcia el jugador y se aleje de el en direccion contraria mientra
+            trans.LookAt(look);                                                                                     // este se encuentre en el campo de vision
+            trans.position -= trans.forward*Time.deltaTime*speed;
         }
 	}
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Player"))   // si el pj entra en el campo de vision se incializa el metodo para alejarse
         {
             lookat = true;
         }
             
     }
     private void OnTriggerExit(Collider other)
-    {
-        lookat = false;
+    { 
+        lookat = false;   // si el pj sale de la vision el npc se detiene
     }
 }
