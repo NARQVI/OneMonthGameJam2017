7578660d856dd662786e9ee50dbc9d7b8eb00d25
MSI\Orion <js.palacios437@uniandes.edu.co>
2017-08-25 08:59:11 -0500

    se agrego una sorpresa alfinal

---
+++ Assets/BossIA.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossIA : MonoBehaviour {
+
+    public float speed;
+    public int attackDmg;
+    public int bossLife;
+    public float attackRange;
+    [SerializeField] private bool active;
+    [SerializeField] private GameObject player;
+    [SerializeField] private float distance;
+    private Animator anim;
+
+    private Transform trans;
+
+	// Use this for initialization
+	void Start () {
+        active = false;
+        player = GameObject.Find("Player");
+        trans = GetComponent<Transform>();
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (active)
+        {
+            distance = Vector3.Distance(trans.position, player.GetComponent<Transform>().position);
+            if(distance > attackRange)
+            {
+
+            Vector3 look = new Vector3(player.transform.position.x, trans.position.y, player.transform.position.z);
+            trans.LookAt(look);
+            trans.position = Vector3.MoveTowards(trans.position, player.GetComponent<Transform>().position, speed);
+            anim.SetBool("walk", true);
+
+            }
+            else
+            {
+                anim.SetBool("walk", false);
+            }
+
+
+
+
+
+
+
+
+        }
+	}
+
+
+
+    public void setActive(bool ss)
+    {
+        active = ss;
+    }
+}
---
+++ Assets/BossStar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossStar : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject  boss;
+	void Start () {
+        boss = GameObject.Find("Boss");
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name.Equals("Player"))
+        {
+            boss.GetComponent<BossIA>().setActive(true);
+        }
+    }
+}
