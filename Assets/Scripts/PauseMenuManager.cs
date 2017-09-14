5d8970913e510a6af1f6eac1badbe812ffeb09e7
juseus03 <juseus@gmail.com>
2017-09-14 16:23:46 -0500

    Arreglo Quit

--- Assets/Scripts/PauseMenuManager.cs
+++ Assets/Scripts/PauseMenuManager.cs
@@ -9,30 +9,52 @@ public class PauseMenuManager : MonoBehaviour {
 	Image bkgImage;
 	GameObject pauseText;
 	bool isPaused;
+	bool isQuit;
 
 	// Use this for initialization
 	void Start () {
-		bkgImage = GameObject.Find ("PauseBgImage").GetComponent<Image>() ;
-		pauseText = GameObject.Find ("PauseText");
-		bkgImage.enabled = false;
-		pauseText.SetActive (false);
-		isPaused = false;
+		SetUp ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		if (isQuit) {
+			if(SceneManager.GetActiveScene().name=="Lvl1")
+				SetUp ();
+		}
+
 		if (Input.GetKeyDown (KeyCode.P)) 
 		{
 			isPaused = !isPaused;
-			bkgImage.enabled = isPaused;
-			pauseText.SetActive (isPaused);
-			Time.timeScale = isPaused ? 0 : 1;
+			Pause ();
 		}
 
 		if (isPaused && Input.GetKeyDown (KeyCode.Q)) {
+			isPaused = false;
+			Pause ();
+			isQuit = true;
 			SceneManager.LoadScene (0);
-			Time.timeScale = 1;
 		}
 	}
+
+
+	void SetUp()
+	{
+		bkgImage = GameObject.Find ("PauseBgImage").GetComponent<Image>() ;
+		pauseText = GameObject.Find ("PauseText");
+		bkgImage.enabled = false;
+		pauseText.SetActive (false);
+		isPaused = false;
+		isQuit = false;
+	}
+
+	void Pause()
+	{
+		Debug.Log (isPaused);
+		bkgImage.enabled = isPaused;
+		pauseText.SetActive (isPaused);
+		Time.timeScale = isPaused ? 0 : 1;
+	}
+		
 }
