04deec1e5045ef147ada4af47d0e6a4fb14b9fec
Blackmagelvl100 <nf.mendoza912@gmail.com>
2017-09-14 14:46:18 -0500

    Muro Escena de entrada y tutorial

---
+++ Assets/Scripts/Tutorial/Tutorial.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour {
+
+
+    public int Order;
+    [TextArea(3,10)]
+    public string Explanation;
+
+	// Use this for initialization
+	void Awake ()
+    {
+
+        TutorialManager.Instance.Tutorials.Add(this);
+       
+
+	}
+	
+    public virtual void CheckifHappening( ){ }
+	
+}
